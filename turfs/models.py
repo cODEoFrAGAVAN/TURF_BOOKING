8e301a782cb3512087b76a703889e733c09079f1
ragavans30zebuetrade <ragavendran.s@zebuetrade.com>
2024-09-16 18:30:08 +0530

    turf module added

---
+++ turfs/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Turf_registration(models.Model):
+    turf_name = models.CharField(max_length=50,null=False)
+    turf_address = models.CharField(max_length=500,null=False)
+    turf_pincode = models.CharField(max_length=6,null=False)
+    turf_mobile_number = models.CharField(max_length=10,primary_key=True,null=False)
+    turf_mailid = models.EmailField(max_length=50,unique=True,null=False)
+    turf_land_line_number = models.CharField(max_length=20,null=True)
+    turf_images = models.CharField(max_length=1000,null=False)
+    registration_date = models.DateField(default=timezone.now)
+    user_name = models.CharField(max_length=20,unique=True,null=False)
+    password = models.CharField(max_length=128,null=False)
+    def __str__(self) -> str:
+        return self.user_name
+
+
+class Forget_turf_password(models.Model):
+    user_name = models.CharField(max_length=20,null=False)
+    mobile_number = models.CharField(max_length=10,primary_key=True,null=False)
+    mailid = models.EmailField(max_length=50,unique=True,null=False)
+    otp = models.CharField(max_length=6,null=False)
+    isvalid = models.CharField(null=False,default='True',max_length=10)
+    def __str__(self) -> str:
+        return self.user_name
+    
--- user/urls.py
+++ user/urls.py
@@ -2,9 +2,9 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('login',login),
-    path('signup',user_signin_up),
-    path('forget_password',forget_user_password),
+    path("login",login),
+    path("signup",user_signin_up),
+    path("forget_password",forget_user_password),
     path("verify_otp",verify_otp),
     path("update_password",update_password)
 ]
